38d6d72c080670383b70d5ec7d2d4f8d6f0437fc
Patricio Yumbillo <pyumbillo75@gmail.com>
2019-01-24 22:57:33 -0500

    Funcional Js

---
+++ funcional-js-00-boilerplate/js/main.js
@@ -0,0 +1,54 @@
+const compose = (...functions) => data =>
+  functions.reduceRight((value, func) => func(value), data)
+let description =$('#description')
+let calories= $('#calories')
+let carbs =$('#carbs')
+let protein =$('#protein')
+let list=[]
+
+description.keypress(()=>{
+  description.removeClass('is-invalid')
+
+})
+calories.keypress(()=>{
+  calories.removeClass('is-invalid')
+
+})
+carbs.keypress(()=>{
+  carbs.removeClass('is-invalid')
+
+})
+protein.keypress(()=>{
+  protein.removeClass('is-invalid')
+
+})
+
+const validateInputs=()=>{
+
+  description.val()?'':description.addClass('is-invalid')
+  calories.val()?'':calories.addClass('is-invalid')
+  carbs.val()?'':carbs.addClass('is-invalid')
+  protein.val()?'':protein.addClass('is-invalid')
+  const add = ()=>{
+    const newItem={
+      description: description.val(),
+      calories: parseInt( calories.val()),
+      carbs: parseInt(carbs.val()),
+      protein:parseInt( protein.val())
+    }
+    list.push(newItem)
+    console.log(list)
+    cleanInputs()
+  }
+  const cleanInputs = ()=>{
+    description.val('')
+    calories.val('')
+    carbs.val('')
+    protein.val('')
+  }
+
+  if(description.val()&& calories.val() && carbs.val()&& protein.val())  add()
+
+
+}
+
